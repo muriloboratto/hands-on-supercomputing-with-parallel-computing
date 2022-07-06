1eedc4b753ae6da69b3aa6afda73ea8f6721e71c
Murilo Boratto <murilo.boratto@fieb.org.br>
2022-07-06 09:38:56 -0300

    Update files

---
+++ Hands-On-3-Portable-Parallel-Programming-with-MPI/solution/paralell-algebraic-function-handson3.c
@@ -0,0 +1,69 @@
+/*
+File:           paralell-algebraic-function-handson3.c
+Date:           20220419
+Purpose:        Parallelize the tridiagonal matrix using MPI
+Author:         Murilo Boratto < muriloboratto 'at' fieb.org.br >
+Usage:
+HowToCompile:   mpicc paralell-algebraic-function-handson3.c -o function -fopenmp
+HowToExecute:   mpirun -np 4 ./function
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define SIZE 10
+
+int main (int argc, char **argv){
+
+	double variableX, variableXRec, parts[4], fx=0, coefficient, result, value=0;
+	char op[] = {'+', '-', '*'}, c;
+	int numberOfProcessors, id, to, tag = 1000;		
+	
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numberOfProcessors);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Status status;
+	
+	switch(id)
+	{	
+		case 0: //master			
+			
+			parts[3]  = atoi(argv[1]); //a
+            parts[2]  = atoi(argv[2]); //b
+            parts[1]  = atoi(argv[3]); //c
+            parts[0]  = atoi(argv[4]); //d
+            variableX = atoi(argv[5]); //x
+
+			for(to = 1; to < numberOfProcessors; to++) { //coefficients
+				MPI_Send(&parts[to], 1, MPI_DOUBLE, to, tag, MPI_COMM_WORLD);
+				MPI_Send(&variableX, 1, MPI_DOUBLE, to, tag, MPI_COMM_WORLD);
+			}
+			
+			for(to = 1; to < numberOfProcessors; to++) {
+				MPI_Recv(&result, 1, MPI_DOUBLE, to, tag, MPI_COMM_WORLD, &status);
+				fx += result;
+			}			
+			fx += parts[0];
+			printf ("f(%f) = %f\n", variableX, fx);
+			break;
+
+		default: //workers
+			MPI_Recv(&coefficient, SIZE, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+			MPI_Recv(&variableXRec, 1, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+
+			value = coefficient;
+			
+			int i;
+			for(i = 0; i < id; i++) 
+				value *= variableXRec;
+				
+			MPI_Send(&value,1,MPI_DOUBLE,0,tag,MPI_COMM_WORLD); //Return the result
+
+	}
+
+	MPI_Finalize();
+	
+	return 0;
+}
