2695695b39b56801fe4b76863120dd4f90a95256
muriloboratto <muriloboratto@gmail.com>
2023-07-19 21:16:48 -0300

    refactor update solution code  handson 5

---
+++ Hands-On-5-Basic-Hybrid-Application-MPI+OpenMP/solution/mm-mpi+openmp.c
@@ -0,0 +1,194 @@
+/*
+File:           mm-mpi+openmp.c
+Purpose:        Parallelize matrix multiplication using OpenMP+MPI
+Authors:        Francisco Almeida, Domingo Giménez, José Miguel Mantas, Antonio M. Vidal
+Usage:
+HowToCompile:   mpicc mm-mpi+openmp.c -o mm-mpi+openmp -fopenmp
+HowToExecute:   mpirun -np <numberOfProcesses> ./mm-mpi+openmp <size> <threads> 
+Example:        mpirun -np         4           ./mm-mpi+openmp  100       16     
+
+Comments:
+                ◆ Spanish code comments;          
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <mpi.h>
+#include <omp.h>
+
+
+void mm(double *a, int fa,int ca,int lda,double *b,int fb,int cb,int ldb,double *c,int fc,int cc,int ldc,int nodo,char *maquina)
+{
+  int i, j, k;
+  double s;
+
+#pragma omp parallel 
+{
+#pragma omp for private(i,j,k,s) schedule(static)
+  for (i = 0; i < fa; i++) 
+  {
+    for(j=0;j<cb;j++)
+    {
+      s=0.;
+      for (k = 0; k < ca; k++)
+	      s = s+a[i*lda+k]*b[k*ldb+j];
+      c[i*ldc+j]=s;
+    }
+  }
+}
+}
+
+/*
+c
+c initialize - random initialization for array
+c
+*/
+
+void initialize(double *m, int f,int c,int ld){
+  int i, j;
+
+  for (i = 0; i < f; i++)
+  {
+    for (j = 0; j < c; j++)
+    {  
+      m[i*ld+j] = (double)(i+j);
+    }
+  }
+}
+
+void initializealea(double *m, int f,int c,int ld){
+  int i, j;
+
+  for (i = 0; i < f; i++)
+  {
+    for (j = 0; j < c; j++)
+    {  
+      m[i*ld+j] = (double)rand()/RAND_MAX;
+    }
+  }
+}
+
+void escribir(double *m, int f,int c,int ld){
+  int i, j;
+
+  for (i = 0; i < f; i++)
+  {
+    for (j = 0; j < c; j++)
+    {  
+      printf("%.4lf ",m[i*ld+j]);
+    }
+    printf("\n");
+  }
+}
+/*
+c
+c     mseconds - returns elapsed milliseconds since Jan 1st, 1970.
+c
+*/
+long long mseconds(){
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return t.tv_sec*1000 + t.tv_usec/1000;
+}
+
+void comparar(double *m1,int fm1,int cm1,int ldm1,double *m2,int fm2,int cm2,int ldm2)
+{
+  int i,j;
+
+  for(i=0;i<fm1;i++)
+    for(j=0;j<cm1;j++)
+    {
+      if(m1[i*ldm1+j]!=m2[i*ldm2+j])
+      {
+        printf("Discrepance in %d,%d: %.8lf , %.8lf\n",i,j,m1[i*ldm1+j],m2[i*ldm2+j]);
+        return;
+      }
+    }
+}
+
+int main(int argc,char *argv[]) {
+  int nodo,np,i, j,fa,fal,ca,lda,fb,cb,ldb,fc,fcl,cc,ldc,N,NUMTHREADS;
+  int long_name;
+  double ti,tf;
+  double *a,*b,*c,*c0;
+  char    nombre_procesador[MPI_MAX_PROCESSOR_NAME];
+  MPI_Status estado;
+ 
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&np);
+  MPI_Comm_rank(MPI_COMM_WORLD,&nodo);
+  MPI_Get_processor_name(nombre_procesador,&long_name);
+
+  if(nodo==0)
+  {
+    N=atoi(argv[1]);
+    NUMTHREADS=atoi(argv[2]);
+  }
+  MPI_Bcast(&N,1,MPI_INT,0,MPI_COMM_WORLD);
+  MPI_Bcast(&NUMTHREADS,1,MPI_INT,0,MPI_COMM_WORLD);
+  omp_set_num_threads(NUMTHREADS);
+  
+  fa=ca=lda=fb=cb=ldb=fc=cc=ldc=N;
+  fal=N/np;
+  fcl=N/np;
+  if(nodo==0)
+  {
+    a = (double *) malloc(sizeof(double)*fa*ca);
+    b = (double *) malloc(sizeof(double)*fb*cb);
+    c = (double *) malloc(sizeof(double)*fc*cc);
+  }
+  else
+  {
+    a = (double *) malloc(sizeof(double)*fal*ca);
+    b = (double *) malloc(sizeof(double)*fb*cb);
+    c = (double *) malloc(sizeof(double)*fcl*cc);
+  }
+  
+  if(nodo==0)
+  {
+    c0=(double *) malloc(sizeof(double)*fc*cc);
+    initialize(a,fa,ca,lda);
+    for(i=1;i<np;i++)
+    {
+      MPI_Send(&a[i*lda*N/np],fal*ca,MPI_DOUBLE,i,20,MPI_COMM_WORLD);
+    }
+    initialize(b,fb,cb,ldb);
+    MPI_Bcast(b,fb*cb,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    mm(a,fa,ca,lda,b,fb,cb,ldb,c0,fc,cc,ldc,nodo,nombre_procesador);
+  }
+  else
+  {
+    MPI_Recv(a,fal*ca,MPI_DOUBLE,0,20,MPI_COMM_WORLD,&estado);
+    MPI_Bcast(b,fb*cb,MPI_DOUBLE,0,MPI_COMM_WORLD);
+  } 
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  ti=MPI_Wtime();
+
+  mm(a,fal,ca,lda,b,fb,cb,ldb,c,fcl,cc,ldc,nodo,nombre_procesador);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  tf=MPI_Wtime();
+  if(nodo==0)
+  {
+    //printf("(%d) Threads %d, Process %d, %s, Time %.6lf\n\n",N, NUMTHREADS, np, nombre_procesador,tf-ti);
+    printf("%d\t%f\n", N, tf-ti);
+    for(i=1;i<np;i++)
+    {
+      MPI_Recv(&c[i*ldc*N/np],fcl*cc,MPI_DOUBLE,i,30,MPI_COMM_WORLD,&estado);
+    }
+  }
+  else
+  {
+    MPI_Send(c,fcl*cc,MPI_DOUBLE,0,30,MPI_COMM_WORLD);
+  } 
+  
+  free(a);
+  free(b);
+  free(c);
+  if(nodo==0)
+    free(c0);
+  MPI_Finalize();
+}
